bb78a81c3ea4ea559cf0d7e0cc23386a3139276e
ganesh_busam <ganesh.busam@zemosolabs.com>
2025-04-24 23:39:07 +0530

    sonar fix

--- src/main/java/com/ganesh/contiq/repository/CustomerRepository.java
+++ src/main/java/com/ganesh/contiq/repository/CustomerRepository.java
@@ -4,7 +4,7 @@ import com.ganesh.contiq.model.Customer;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface CustomerRepository extends ElasticsearchRepository<Customer,Long> {
     Iterable<Customer> findAllByFirstName();
 }
